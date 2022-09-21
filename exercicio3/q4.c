9d57619847e5bee04367f084d4d9050b69febdc4
Kaylanne Santos <102864770+kaylannemendes@users.noreply.github.com>
2022-09-20 22:26:37 -0300

    Update q4.c

--- exercicio3/q4.c
+++ exercicio3/q4.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-main() {
-  char nome;
-  nome = 2;
-  nome = 'a';
-  printf("%c", nome);
+int main() {
+  int valor1, valor2;
+  valor1 = 1.1;
+  valor2 = 1;
+    if(valor1 == valor2){
+      printf("verdadeiro");
+    }else{
+      printf("falso");
+    }
     return 0;
 }
- 
