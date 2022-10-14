3bbf73d6ba713e5eb2ae5b5f4561977c576d719b
Kaylanne Santos <102864770+kaylannemendes@users.noreply.github.com>
2022-10-13 22:15:41 -0300

    Update q3.ts

--- exercicio5/q3.ts
+++ exercicio5/q3.ts
@@ -1,12 +1,1 @@
-class Banco {
-contas: Conta[] = [];
 
-inserir(conta: Conta): void {}
-alterar(conta: Conta): void {}
-excluir(numero: string): void{}
-//consultar(numero: string): Conta {}
-
-sacar(numero: string, valor: number): void {}
-depositar(numero: string, valor: number): void {}
-transfeir(numeroOrigem: string, numeroDestino: String, valor: number): void {}
-}
